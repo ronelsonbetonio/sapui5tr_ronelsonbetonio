88933fb375cda0452f499b2ceb960feffa135010
Ronelson Betonio <ronelson.betonio@accenture.com>
2023-01-30 05:38:30 +0000

    First Commit

---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["zbtp/day6exercise/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"zbtp/day6exercise/test/unit/controller/Overview.controller"
+], function () {
+	"use strict";
+});
